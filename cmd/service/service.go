4dd2ba997866c2bd42cc98c3cb3fd4ea12131df4
tecchu11 <agdjmp.ajtt@gmail.com>
2023-08-13 00:57:28 +0900

    reimpl preauth

--- cmd/service/service.go
+++ cmd/service/service.go
@@ -7,7 +7,6 @@ import (
 
 	"go-playground/internal/transport_layer/rest/handler"
 	"go-playground/internal/transport_layer/rest/middleware"
-	"go-playground/internal/transport_layer/rest/preauth"
 	"go-playground/pkg/renderer"
 	"go.uber.org/zap"
 )
@@ -16,8 +15,13 @@ import (
 func New(env string, logger *zap.Logger, prop *configs.ApplicationProperties, nrApp *newrelic.Application) *chi.Mux {
 	// init misc
 	failure := renderer.NewFailure(middleware.RequestID)
+	preAuth := middleware.PreAuthenticatedUsers(make(map[string]middleware.AuthUser))
+	for _, v := range prop.AuthConfigs {
+		r, _ := middleware.UserRoleString(v.RoleStr) // ignore error
+		preAuth[v.Key] = middleware.AuthUser{Name: v.Name, Role: r}
+	}
 	// init middleware
-	authMiiddleware := middleware.Authenticator(logger, preauth.NewAuthenticationManager(prop.AuthConfigs), failure)
+	generalAuth := preAuth.Auth(logger, failure, map[middleware.UserRole]struct{}{middleware.Admin: {}, middleware.User: {}})
 	// init handler
 	helloHandler := handler.NewHelloHandler(logger, failure)
 	// init mux
@@ -31,7 +35,7 @@ func New(env string, logger *zap.Logger, prop *configs.ApplicationProperties, nr
 		r.Get("/", handler.StatusHandler())
 	})
 	mux.Route("/hello", func(r chi.Router) {
-		r.Use(authMiiddleware)
+		r.Use(generalAuth)
 		r.Get("/", helloHandler.GetName())
 	})
 	return mux
---
+++ internal/transport_layer/rest/middleware/preauth.go
@@ -0,0 +1,96 @@
+package middleware
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"go-playground/pkg/renderer"
+	"net/http"
+
+	"go.uber.org/zap"
+)
+
+// UserRole replesents user role to perform to request.
+type UserRole int
+
+const (
+	// Admin is super user
+	Admin UserRole = iota + 1
+	// User is role for general purpose.
+	User
+)
+
+var (
+	roleStrings = map[UserRole]string{Admin: "Admin", User: "User"}
+	stringRoles = map[string]UserRole{"Admin": Admin, "User": User}
+)
+
+// String convert UserRole to string type.
+// UserRole.Admin to "Admin", UserRole.User to "User"
+func (ur UserRole) String() string {
+	return roleStrings[ur]
+}
+
+// UserRoleString lookup UserRole by given string.
+func UserRoleString(v string) (UserRole, error) {
+	ur, ok := stringRoles[v]
+	if !ok {
+		return 0, fmt.Errorf("role %s does not defined", v)
+	}
+	return ur, nil
+}
+
+type (
+	// AuthUser replesents pre authenticated user.
+	AuthUser struct {
+		Name string
+		Role UserRole
+	}
+	// PreAuthenticatedUsers replesents pre authenticated user map.
+	// Please set key as token that authenticated user's.
+	PreAuthenticatedUsers map[string]AuthUser
+	authCtxKey            struct{}
+)
+
+const (
+	authHeader = "Authorization"
+)
+
+var (
+	NoUser    = AuthUser{}
+	ErrNoUser = errors.New("there is no auth use")
+)
+
+// Auth middleware for authentication and authorization by pre-authenticated token and given permitRoles.
+func (u PreAuthenticatedUsers) Auth(logger *zap.Logger, failure renderer.Failure, permitRoles map[UserRole]struct{}) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		fn := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			user, ok := u[r.Header.Get(authHeader)]
+			if !ok {
+				failure.Response(w, r, http.StatusUnauthorized, "Request With No Authentication", "Request token was not found in your request header")
+				return
+			}
+			_, ok = permitRoles[user.Role]
+			if !ok {
+				failure.Response(w, r, http.StatusUnauthorized, "Request With No Authorization", fmt.Sprintf("Your role(%s) was not performing to action", user.Role.String()))
+				return
+			}
+			next.ServeHTTP(w, r.WithContext(user.Set(r.Context())))
+		})
+		return fn
+	}
+}
+
+// CurrentUser retreive from AuthUser from context.
+func CurrentUser(ctx context.Context) (AuthUser, error) {
+	user, ok := ctx.Value(authCtxKey{}).(AuthUser)
+	if !ok || user == NoUser {
+		return NoUser, ErrNoUser
+	}
+	return user, nil
+}
+
+// Set AuthUser to request context.
+func (u AuthUser) Set(ctx context.Context) context.Context {
+	return context.WithValue(ctx, authCtxKey{}, u)
+}
