e78410c9debc04818624290ffc11b6029b234628
Tetsu Yamakawa <68310167+tecchu11@users.noreply.github.com>
2024-08-01 01:02:13 +0900

    add applier (#145)

---
+++ cmd/api/internal/datasource/maindb/sqldb_test.go
@@ -0,0 +1,44 @@
+package maindb_test
+
+import (
+	"go-playground/cmd/api/internal/datasource/maindb"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewDB(t *testing.T) {
+	tests := map[string]struct {
+		envMap      map[string]string
+		expectError bool
+	}{
+		"success": {
+			envMap: map[string]string{
+				"DB_USER":     "test_user",
+				"DB_PASSWORD": "test_password",
+				"DB_ADDRESS":  "localhost:3306",
+				"DB_NAME":     "test_db",
+			},
+		},
+		"env is unset": {
+			expectError: true,
+		},
+	}
+
+	for k, v := range tests {
+		t.Run(k, func(t *testing.T) {
+			for key, val := range v.envMap {
+				t.Setenv(key, val)
+			}
+			db, err := maindb.NewDB(os.LookupEnv)
+			if v.expectError {
+				require.Error(t, err)
+				require.Nil(t, db)
+			} else {
+				require.NoError(t, err)
+				require.NotNil(t, db)
+			}
+		})
+	}
+}
