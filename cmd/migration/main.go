9009718bf549e75d9edf01fe8ee8a06288d3aa90
Tetsu Yamakawa <68310167+tecchu11@users.noreply.github.com>
2024-08-18 01:51:12 +0900

    use pkg/env/v2 (#167)

--- cmd/migration/main.go
+++ cmd/migration/main.go
@@ -1,39 +1,30 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"database/sql"
-	_ "embed"
-	"errors"
 	"flag"
 	"fmt"
+	"go-playground/pkg/env/v2"
 	"go-playground/pkg/migration"
 	"go-playground/pkg/timex"
-	"os"
-	"strings"
 
 	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	flag.Parse()
-	var (
-		mode = flag.Arg(0)
-		file = flag.Arg(1)
-	)
-
-	envs, err := loadDotEnv(file)
-	if err != nil {
-		panic(err)
+	mode := flag.Arg(0)
+	if mode != "up" && mode != "down" {
+		panic(`mode must be "up" or "down"`)
 	}
-	mysql.NewConfig()
+	applier := env.New(nil)
 	conf := mysql.Config{
-		User:      envs["DB_USER"],
-		Passwd:    envs["DB_PASSWORD"],
+		User:      applier.String("DB_USER"),
+		Passwd:    applier.String("DB_PASSWORD"),
 		Net:       "tcp",
-		Addr:      envs["DB_HOST"],
-		DBName:    envs["DB_NAME"],
+		Addr:      applier.String("DB_ADDRESS"),
+		DBName:    applier.String("DB_NAME"),
 		Loc:       timex.JST(),
 		ParseTime: true,
 	}
@@ -56,33 +47,4 @@ func main() {
 		err := fmt.Errorf(`mode must be "up" or "down"`)
 		panic(err)
 	}
-}
-
-func loadDotEnv(file string) (map[string]string, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	scanner := bufio.NewScanner(f)
-	envs := make(map[string]string)
-	for scanner.Scan() {
-		line := scanner.Text()
-
-		if strings.TrimSpace(line) == "" || strings.HasPrefix(line, "#") || !strings.HasPrefix(line, "DB_") {
-			continue
-		}
-		kv := strings.SplitN(line, "=", 2)
-		if len(kv) != 2 {
-			return nil, errors.New("unexpected .env format")
-		}
-		key := strings.TrimSpace(kv[0])
-		value := strings.TrimSpace(kv[1])
-		envs[key] = value
-	}
-	err = scanner.Err()
-	if err != nil {
-		return nil, err
-	}
-	return envs, nil
 }
