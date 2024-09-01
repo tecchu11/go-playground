000299e4f409520e046faf1460153265c6871b98
Tetsu Yamakawa <68310167+tecchu11@users.noreply.github.com>
2024-09-01 19:08:41 +0900

    rm (#188)

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,28 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-BLUE="\033[0;34m"
-RED="\033[0;31m"
-NC="\033[0m"
-
-# Install Zinit (See https://github.com/zdharma-continuum/zinit)
-echo "$BLUE [INFO] Install Zinit $NC"
-bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
-zinit self-update
-
-# link dotfiles
-WORK_DIR="$(cd "$(dirname "$0")" && pwd )"
-echo "$BLUE [INFO] Link dotfiles on $WORK_DIR $NC"
-for dotfile in "$WORK_DIR"/.??* ; do
-  [ "$dotfile" = "$WORK_DIR"/postCreateCommand.sh ] && continue
-  [ "$dotfile" = "$WORK_DIR"/devcontainer.json ] && continue
-  ln -fnsv "$dotfile" "$HOME"
-done
-echo "$BLUE [INFO] Locate dotfiles $NC"
-
-## setup asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
-
-## setup completions
+# setup github-cli completion
 gh completion -s zsh > /usr/local/share/zsh/site-functions/_gh
-
-echo "$BLUE [INFO] Install Done. Please run source ~/.zshrc $NC"
