7aa1650524aea439a7521daf91199fb51a00fee1
tecchu11 <agdjmp.ajtt@gmail.com>
2025-02-13 14:16:25 +0000

    use go tool directive

---
+++ tools/tool.go
@@ -0,0 +1,12 @@
+//go:build tools
+
+// Package tools provides all tolls for this app.
+//
+// tool.go exists so that all tools can be managed by Renovate.
+package tools
+
+import (
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen"
+	_ "github.com/sqlc-dev/sqlc/cmd/sqlc"
+)
