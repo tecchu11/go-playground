87258b673182cc5eed9fe174b59f791f4127760a
tecchu11 <agdjmp.ajtt@gmail.com>
2025-02-19 16:43:48 +0000

    fix

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+sudo chown -R $(whoami): /home/vscode/.cache
+sudo chown -R $(whoami): /home/vscode/.config/gh
 # setup github-cli completion 
 sudo gh completion -s zsh | sudo tee /usr/local/share/zsh/site-functions/_gh > /dev/null
